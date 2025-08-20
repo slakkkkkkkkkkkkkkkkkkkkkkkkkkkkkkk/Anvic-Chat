40b34239931ba2b3c3dd3219ced89148151bb97e
onspace.ai <onspace.ai@gmail.com>
2025-08-20 08:06:22 -0300

    Fix Expo Router compilation issues

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,11 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Ensure proper resolution of modules
+config.resolver.alias = {
+  '@': './src',
+};
+
+config.resolver.platforms = ['ios', 'android', 'web'];
+
 module.exports = config;
